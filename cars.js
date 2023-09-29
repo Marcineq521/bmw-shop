a7450fe00e2e2d3ac1d0372abe60b213e483ab77
Marcin <marcinbaran789@gmail.com>
2023-09-29 20:10:37 +0200

    newModify

--- cars.js
+++ cars.js
@@ -12,7 +12,7 @@ const cars=[
       {
         id: 1,
         name: "BMW iX",
-        description: ` sdffgdgdf ipsum dolor sit amet consectetur adipisicing elit. Quod,
+        description: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod,
         dolores eaque. Eligendi ab officia minus.`,
         price: 29999.99,
         image: "./images/2.png",
